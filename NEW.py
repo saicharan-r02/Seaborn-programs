2d5eb6e066a4b1073f2c35cf48961ed2f997f967
saicharan-o <saicharanr227@gmail.com>
2026-04-07 21:43:31 +0530

    CODE for reversed program

---
+++ NEW.py
@@ -0,0 +1,11 @@
+s = "abc"
+total = 0
+
+for i in range(len(s)):
+    reversed_pos = 26 - (ord(s[i]) - ord('a'))
+    print(ord(s[i]) - ord('a'))
+    print(reversed_pos)
+    total += reversed_pos * (i + 1)
+
+print(total)
+print(ord('b'))
