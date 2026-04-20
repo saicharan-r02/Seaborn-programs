968a674a96f76fea1821b299c76899fbe76de0f0
saicharan-o <saicharanr227@gmail.com>
2026-04-20 17:56:28 +0530

    Fixing Joint file

--- Joint.py
+++ Joint.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 
 plt.style.use("seaborn-v0_8")
+
 d=sn.load_dataset("titanic")
 print(d.head())
 
@@ -18,5 +19,6 @@ sn.jointplot(x="age",y="fare",kind="hex",data=d)
 sn.jointplot(x="age",y="fare",kind="reg",data=d)
 
 dt= d.dropna(subset=["age", "fare"])
+
 sn.jointplot(x="age",y="fare",data=dt)
 plt.show()
