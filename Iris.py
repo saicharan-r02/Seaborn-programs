ca78a06c28b09f4566c9d1281a7f21f7f1a1adbc
saicharan-o <saicharanr227@gmail.com>
2026-04-20 17:54:41 +0530

    changes in Iris file

--- Iris.py
+++ Iris.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 
 iris = load_iris()
+
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 df["species"] = iris.target
 
