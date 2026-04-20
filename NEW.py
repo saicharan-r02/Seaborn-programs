2eb205b9778021aeda389b786d7f08313e19a3d6
saicharan-o <saicharanr227@gmail.com>
2026-04-20 17:57:33 +0530

    Fixing the New PYTHON file

--- NEW.py
+++ NEW.py
@@ -3,8 +3,10 @@ total = 0
 
 for i in range(len(s)):
     reversed_pos = 26 - (ord(s[i]) - ord('a'))
+
     print(ord(s[i]) - ord('a'))
     print(reversed_pos)
+    
     total += reversed_pos * (i + 1)
 
 print(total)
