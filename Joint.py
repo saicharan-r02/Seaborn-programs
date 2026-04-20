d39ff7ded2aa57aa1a6efd5324f8034827b1523c
saicharan-o <saicharanr227@gmail.com>
2026-04-20 17:55:22 +0530

    changes in Joint

--- Joint.py
+++ Joint.py
@@ -2,14 +2,17 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
+
 plt.style.use("seaborn-v0_8")
 d=sn.load_dataset("titanic")
 print(d.head())
+
 sn.jointplot(x="age",y="fare",data=d)
 sn.jointplot(x="age",y="fare",hue="survived",data=d)
 sn.jointplot(x="age",y="fare",kind="kde",data=d)
 sn.jointplot(x="age",y="fare",kind="hex",data=d)
 sn.jointplot(x="age",y="fare",kind="reg",data=d)
+
 dt= d.dropna(subset=["age", "fare"])
 sn.jointplot(x="age",y="fare",data=dt)
 plt.show()
