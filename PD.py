110644111d5718f7cb43c4ccec7a9ad1d1aa79c1
saicharan-o <saicharanr227@gmail.com>
2026-05-13 16:05:24 +0530

    changes in PD file

--- PD.py
+++ PD.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
 import warnings
+
 warnings.filterwarnings("ignore")
 plt.style.use("seaborn-v0_8")
+
 d=sn.load_dataset("penguins")
 print(d.head())
+
 sn.pairplot(d)
 sn.pairplot(d,hue="species")
 sn.pairplot(d,vars=["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"],hue="species")
