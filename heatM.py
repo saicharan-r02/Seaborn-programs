9ff5994d456ba65cdf33d0aa258fda72930e4e82
saicharan <saicharanr227@gmail.com>
2026-06-23 18:45:20 +0530

    Refactor heatmap visualizations in heatM.py
    Removed duplicate heatmap visualizations and adjusted formatting.

--- heatM.py
+++ heatM.py
@@ -5,15 +5,12 @@ import pandas as pd
 
 plt.style.use("seaborn-v0_8")
 d=sn.load_dataset("fmri")
-
 print(d.head())
 
 f= d.pivot_table(index="timepoint",columns="event",values="signal",aggfunc="mean")
 sn.heatmap(f)
 plt.show()
-
 sn.heatmap(f,cbar=True,linewidths=0.5,annot=True,fmt=".2f")
 plt.show()
-
 sn.heatmap(f,cbar=False,linewidths=0.5,annot=True,fmt=".2f",cmap="magma")
 plt.show()
